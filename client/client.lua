dc8b0a8f21bc222b5a265f0ce89ac31802740714
Jordi Xavier <harpymclol@gmail.com>
2024-07-29 23:34:29 +0200

    Fixed class
    Formated client.lua class

--- client/client.lua
+++ client/client.lua
@@ -32,60 +32,64 @@ function openInventory()
 		local filteredItems = {}
 
 		for _, item in ipairs(items) do
-            if (Config.ItemCategories.Consumables[item.name]) then
-                table.insert(filteredItems, {
-                    name = item.name,
-                    label = item.label,
-                    weight = item.weight,
-                    count = item.count,
-                    category = "consumables"
-                })
-            end
-            if (Config.ItemCategories.Drugs[item.name]) then
-                table.insert(filteredItems, {
-                    name = item.name,
-                    label = item.label,
-                    weight = item.weight,
-                    count = item.count,
-                    category = "drugs"
-                })
-            end
-            if (Config.ItemCategories.Weapons[item.name]) then
-                table.insert(filteredItems, {
-                    name = item.name,
-                    label = item.label,
-                    weight = item.weight,
-                    count = item.count,
-                    category = "weapons"
-                })
-            end
-            if (not Config.ItemCategories.Consumables[item.name] and not Config.ItemCategories.Drugs[item.name] and not Config.ItemCategories.Weapons[item.name]) then
-                table.insert(filteredItems, {
-                    name = item.name,
-                    label = item.label,
-                    weight = item.weight,
-                    count = item.count,
-                    category = "other"
-                })
-            end
+			if Config.ItemCategories.Consumables[item.name] then
+				table.insert(filteredItems, {
+					name = item.name,
+					label = item.label,
+					weight = item.weight,
+					count = item.count,
+					category = "consumables",
+				})
+			end
+			if Config.ItemCategories.Drugs[item.name] then
+				table.insert(filteredItems, {
+					name = item.name,
+					label = item.label,
+					weight = item.weight,
+					count = item.count,
+					category = "drugs",
+				})
+			end
+			if Config.ItemCategories.Weapons[item.name] then
+				table.insert(filteredItems, {
+					name = item.name,
+					label = item.label,
+					weight = item.weight,
+					count = item.count,
+					category = "weapons",
+				})
+			end
+			if
+				not Config.ItemCategories.Consumables[item.name]
+				and not Config.ItemCategories.Drugs[item.name]
+				and not Config.ItemCategories.Weapons[item.name]
+			then
+				table.insert(filteredItems, {
+					name = item.name,
+					label = item.label,
+					weight = item.weight,
+					count = item.count,
+					category = "other",
+				})
+			end
 		end
 
 		SendNUIMessage({
 			action = "open",
 			data = {
-                inventory = {
-                    open = true,
-                    items = filteredItems,
-                },
-                money = cb.money,
-                accounts = cb.accounts,
-                weapons = cb.weapons,
-                weight = cb.weight,
-                maxWeight = cb.maxWeight,
-            },
+				inventory = {
+					open = true,
+					items = filteredItems,
+				},
+				money = cb.money,
+				accounts = cb.accounts,
+				weapons = cb.weapons,
+				weight = cb.weight,
+				maxWeight = cb.maxWeight,
+			},
 		})
 
 		SetNuiFocus(true, true)
-        SetNuiFocusKeepInput(true)
+		SetNuiFocusKeepInput(true)
 	end, GetPlayerServerId(PlayerId()))
 end
